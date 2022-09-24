7841c59a7a34f40f4328cba31880d43c68114244
ozanko <ozankyncu@gmail.com>
2022-09-24 13:58:19 +0300

    Updating to use maven central BOM

--- src/main/java/com/kyncu/msbeerorderservice/web/mappers/BeerOrderLineMapper.java
+++ src/main/java/com/kyncu/msbeerorderservice/web/mappers/BeerOrderLineMapper.java
@@ -4,7 +4,6 @@ import com.kyncu.msbeerorderservice.domain.BeerOrderLine;
 import com.kyncu.msbeerorderservice.web.model.BeerOrderLineDto;
 import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(uses = {DateMapper.class})
 @DecoratedWith(BeerOrderLineMapperDecorator.class)
